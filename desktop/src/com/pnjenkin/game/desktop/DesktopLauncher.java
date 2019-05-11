2881e9facebc421de310d76ad29330a1fdcdf823
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-11 10:40:16 +0100

    Initial commit (after libGDX setup jar run)

---
+++ desktop/src/com/pnjenkin/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pnjenkin.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pnjenkin.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
