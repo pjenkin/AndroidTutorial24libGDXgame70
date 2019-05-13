1b168cf56cd174e333e3e39aee4e04e45a504382
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-13 10:44:01 +0100

    72 using FreeTypeFont

--- core/src/com/pnjenkin/game/PNJGdxGame.java
+++ core/src/com/pnjenkin/game/PNJGdxGame.java
@@ -29,16 +29,16 @@ public class PNJGdxGame extends ApplicationAdapter {
 		freeTypeFontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/timesnewroman.ttf"));
 		// add ttf file manually in new fonts within android/assets
 		freeTypeFontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-		freeTypeFontParameter.size = 12;
+		freeTypeFontParameter.size = 72;
 		freeTypeFontParameter.color = Color.RED;
-		freeTypeFontParameter.characters = "Hello there";
+//		freeTypeFontParameter.characters = "Hello there";
 		bitmapFont = freeTypeFontGenerator.generateFont(freeTypeFontParameter);
 		// use freetype font https://stackoverflow.com/a/40739429/11365317
 		// best to specify this from start - very hard to retrospectively add this library
 
-		bitmapFont = new BitmapFont();
-		bitmapFont.setColor(Color.BLACK); // this is deprecated, should by 2019 be using FreeType, but...
-		bitmapFont.getData().setScale(5);	// default font size small on hi-res screen, so scale-up
+//		bitmapFont = new BitmapFont();
+//		bitmapFont.setColor(Color.BLACK); // this is deprecated, should by 2019 be using FreeType, but...
+//		bitmapFont.getData().setScale(5);	// default font size small on hi-res screen, so scale-up
 
 		// NB Screen expected to be in landscape format by libGDX by default https://stackoverflow.com/a/35614010/11365317
 
@@ -64,7 +64,8 @@ public class PNJGdxGame extends ApplicationAdapter {
 		spriteBatch.setProjectionMatrix(orthographicCamera.combined);
 		// Drawing happening below
 		spriteBatch.begin();
-		bitmapFont.draw(spriteBatch, "Durdadhewhi, fatla genowdh whi?", 100,101);	// origin is bottom-left
+		bitmapFont.draw(spriteBatch, "Durdadhewhi, fatla genowgh whi?", 0,100);
+		// Origin of screen is bottom-left, so a y of zero is likely to be off-screen/invisible
 		spriteBatch.end();
 
 	}
