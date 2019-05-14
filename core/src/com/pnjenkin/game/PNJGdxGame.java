3a20a380cb7070185f45c7259f5e002bd11254bc
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-14 13:44:31 +0100

    75 User touch on screen handled with message printed in centre

--- core/src/com/pnjenkin/game/PNJGdxGame.java
+++ core/src/com/pnjenkin/game/PNJGdxGame.java
@@ -2,12 +2,14 @@ package com.pnjenkin.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -16,7 +18,7 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 import javax.xml.soap.Text;
 
-public class PNJGdxGame extends ApplicationAdapter {
+public class PNJGdxGame extends ApplicationAdapter implements InputProcessor {
 	// REM out all boilerplate code - class variables and inside methods
 //	SpriteBatch batch;
 //	Texture img;
@@ -32,6 +34,10 @@ public class PNJGdxGame extends ApplicationAdapter {
 //	private Animation animation;
 	private Animation<TextureRegion> animation;
 	private float timePassed = 0;		// used to determine where in sequence
+	// for handling user input (75)
+	private int screenWidth, screenHeight;
+	private String message = "Touch to activate";
+	GlyphLayout glyphLayout;
 
 	@Override
 	public void create () {
@@ -61,6 +67,12 @@ public class PNJGdxGame extends ApplicationAdapter {
 		animation = new Animation<TextureRegion>(1/30f, shooterAtlas.getRegions());
 		// 1/30f for 30 fps
 
+		// 75 handling user input
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
+
+		Gdx.input.setInputProcessor(this);		// look in this class itself for input methods
+
 	}
 
 	@Override
@@ -79,6 +91,15 @@ public class PNJGdxGame extends ApplicationAdapter {
 		orthographicCamera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		orthographicCamera.update();
 
+		// for use with user input
+		glyphLayout = new GlyphLayout();		// getBounds deprecated, so use GlypthLayout
+		glyphLayout.setText(bitmapFont, message);
+		float textWidth = glyphLayout.width;
+		float textHeight = glyphLayout.height;
+
+		float x = screenWidth / 2 - textWidth /2;
+		float y = screenHeight / 2 - textHeight /2;
+
 		Sprite sprite = new Sprite(image);	// initialise sprite
 
 		spriteBatch = new SpriteBatch();
@@ -91,8 +112,10 @@ public class PNJGdxGame extends ApplicationAdapter {
 		// for 73 draw image
 //		sprite.draw(spriteBatch);
 		// for 74 animation
-		timePassed += Gdx.graphics.getDeltaTime();		// use gdx library for time
-		spriteBatch.draw(animation.getKeyFrame(timePassed, true), 100, 100);
+//		timePassed += Gdx.graphics.getDeltaTime();		// use gdx library for time
+//		spriteBatch.draw(animation.getKeyFrame(timePassed, true), 100, 100);
+		// for 75 handling user input
+		bitmapFont.draw(spriteBatch,message, x, y);		// calculated to draw centred in screen
 		spriteBatch.end();
 	}
 	
@@ -104,6 +127,113 @@ public class PNJGdxGame extends ApplicationAdapter {
 		bitmapFont.dispose();
 		freeTypeFontGenerator.dispose();
 		image.dispose();		// remember to dispose of everything!
-		shooterAtlas.dispose();		// remember to dispose of everything!
+		shooterAtlas.dispose();		// remember to dispose of everything! (anything implementing Disposable)
+	}
+
+	// manually move touch events to top here - since focussing on mobile device (not desktop) only
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		message = "Touched down at " + screenX + ", " + screenY;
+		return true;
+//		return false;
+	}
+
+	/**
+	 * Called when a finger was lifted or a mouse button was released. The button parameter will be {link Buttons#LEFT} on iOS.
+	 *
+	 * @param screenX
+	 * @param screenY
+	 * @param pointer the pointer for the event.
+	 * @param button  the button
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		message = "Touched up at " + screenX + ", " + screenY;
+		return true;
+//		return false;
+	}
+
+	/**
+	 * Called when a finger or the mouse was dragged.
+	 *
+	 * @param screenX
+	 * @param screenY
+	 * @param pointer the pointer for the event.
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// touchDragged fired a lot! by accidental dragging, so don't really implement touchDragged at the mo
+		return false;
+	}
+
+// end of touch events
+
+
+	/**
+	 * Called when a key was pressed
+	 *
+	 * @param keycode one of the constants in {link Input.Keys}
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	/**
+	 * Called when a key was released
+	 *
+	 * @param keycode one of the constants in {link Input.Keys}
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	/**
+	 * Called when a key was typed
+	 *
+	 * @param character The character
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	/**
+	 * Called when the screen was touched or a mouse button was pressed. The button parameter will be {@link Buttons#LEFT} on iOS.
+	 *
+	 * @param screenX The x coordinate, origin is in the upper left corner
+	 * @param screenY The y coordinate, origin is in the upper left corner
+	 * @param pointer the pointer for the event.
+	 * @param button  the button
+	 * @return whether the input was processed
+	 */
+
+	/**
+	 * Called when the mouse was moved without any buttons being pressed. Will not be called on iOS.
+	 *
+	 * @param screenX
+	 * @param screenY
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	/**
+	 * Called when the mouse wheel was scrolled. Will not be called on iOS.
+	 *
+	 * @param amount the scroll amount, -1 or 1 depending on the direction the wheel was scrolled.
+	 * @return whether the input was processed.
+	 */
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
